dfd5a79f4587807a47b1356bd919a75291f9e0fd
MatDat <math03t7@stud.kea.dk>
2024-09-17 12:53:42 +0200

    first commit

---
+++ src/components/DayColumns.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./css/DayColumns.css";
+
+const DayColumns = () => {
+  return (
+    <div className="day-columns">
+      <div className="day-column">Monday</div>
+      <div className="day-column">Tuesday</div>
+      <div className="day-column">Wednesday</div>
+      <div className="day-column">Thursday</div>
+      <div className="day-column">Friday</div>
+    </div>
+  );
+};
+
+export default DayColumns;
